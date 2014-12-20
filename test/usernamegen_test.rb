dfa340fd226a5b21e7716d64adceb77be27bff27
Sven Pachnit <sven@bmonkeys.net>
2014-12-21 00:35:41 +0100

    Improved test to not fail after every change to the wordlists

--- test/usernamegen_test.rb
+++ test/usernamegen_test.rb
@@ -2,9 +2,11 @@ require "minitest/autorun"
 require "usernamegen"
 
 class TestUsernamegen < Minitest::Test
-
-  def test_one
-    assert_equal "Aquamarine Monkey", Usernamegen.one(rng: Random.new(1))
+  def test_random_factor
+    100.times do # <-- just to make sure ;)
+      t = Time.now.to_i
+      assert_equal Usernamegen.one(rng: Random.new(t)), Usernamegen.one(rng: Random.new(t))
+      refute_equal Usernamegen.one(rng: Random.new(t)), Usernamegen.one(rng: Random.new(t + 1))
+    end
   end
-
 end
