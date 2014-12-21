736a21c71640e5ce3efa492976fde0d9cb6acef6
Sven Pachnit <sven@bmonkeys.net>
2014-12-21 02:57:47 +0100

    Reduce test iterations

--- test/usernamegen_test.rb
+++ test/usernamegen_test.rb
@@ -3,7 +3,7 @@ require "usernamegen"
 
 class TestUsernamegen < Minitest::Test
   def test_random_factor
-    100.times do # <-- just to make sure ;)
+    10.times do # <-- just to make sure ;)
       t = Time.now.to_i
       assert_equal Usernamegen.one(rng: Random.new(t)), Usernamegen.one(rng: Random.new(t))
       refute_equal Usernamegen.one(rng: Random.new(t)), Usernamegen.one(rng: Random.new(t + 1))
